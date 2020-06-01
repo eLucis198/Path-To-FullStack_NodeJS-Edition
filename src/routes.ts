ab48c45e719c6a07f21f6e868b969b480a2a286b
Guilherme Guedes <30610742+eLucis198@users.noreply.github.com>
2020-06-01 17:59:05 -0300

    Added PORT to .env.example file.

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const routes = Router()
 
 routes.get('/', (req, res) => {
-  return res.send('Hello World')
+  return res.send('Hello World 2')
 })
 
 export default routes
