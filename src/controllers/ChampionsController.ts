09dec794e42aa025c2ed7cd42a91eaf523efe298
Guilherme Guedes <30610742+eLucis198@users.noreply.github.com>
2020-06-04 11:20:34 -0300

    Update .env.example

--- src/controllers/ChampionsController.ts
+++ src/controllers/ChampionsController.ts
@@ -11,18 +11,6 @@ class ChampionsController {
     const champions = await knex('champion').select('*').where('id', id)
     return res.json(champions)
   }
-
-  public async create (req: Request, res: Response): Promise<Response> {
-    return res.send('Hello World create')
-  }
-
-  public async update (req: Request, res: Response): Promise<Response> {
-    return res.send('Hello World udpate')
-  }
-
-  public async delete (req: Request, res: Response): Promise<Response> {
-    return res.send('Hello World delete ')
-  }
 }
 
 export default ChampionsController
