9f0b6bdbc7c107067b9c5ff06c714f294e0a73c5
Guilherme Guedes <30610742+eLucis198@users.noreply.github.com>
2020-06-24 21:41:09 -0300

    Delete everything to try the guide and see if its good.

--- src/database/connection.ts
+++
@@ -1,6 +0,0 @@
-import knex from 'knex'
-import { development, staging } from '../../knexfile'
-
-const connection = knex(process.env.NODE_ENV === 'development' ? development : staging)
-
-export default connection
--- src/database/old_connection.ts
+++
@@ -1,8 +0,0 @@
-import knex from 'knex'
-
-const connection = knex({
-  client: process.env.DB_CLIENT,
-  connection: process.env.DB_URL
-})
-
-export default connection
